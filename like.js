c7f4d2af124b048b551a44c9c86dbef3a98c6d3b
welintonv <welintoncesardevito@gmail.com>
2021-09-17 02:05:41 -0300

    Atualização

--- like.js
+++ like.js
@@ -1,18 +1,26 @@
 String.prototype.like = function(s){
     texto = this.toUpperCase();
+    texto = texto.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+
     s = s.toUpperCase();
+    if(s.trim() === "" || s.trim() == "*") return true;
+    if(!s.includes("*")) return texto.includes(s);
+
     let arr = s.split("*");
+    arr = arr.map((v) => {
+      return v.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+    });
 
     let str = arr[0];
-    if(str.trim() != "") 
-        if(texto.substr(0,str.length) != str) 
+    if(str.trim() != "")
+        if(texto.substr(0,str.length) != str)
             return false;
 
     str = arr[arr.length - 1];
-    if(str.trim() != "") 
-        if(texto.substr(-str.length) != str) 
+    if(str.trim() != "")
+        if(texto.substr(-str.length) != str)
             return false;
-    
+
     if(arr.length == 1) return true;
 
     let i = 1;
